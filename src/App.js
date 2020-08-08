e52f160d6d43c2750bb114dd7859e0911de662d4
Ariel Velasco <arield.velasco@gmail.com>
2020-08-08 17:48:36 -0300

    confirmando que funca

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          testing
+          confirmando que funca
         </a>
       </header>
     </div>
